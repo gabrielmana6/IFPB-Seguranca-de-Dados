df6c3418f41e98b2d4c4b9e89e89bab2735c2ffc
Gabriel Manassés <gabrielmanasses13@gmail.com>
2023-04-05 03:06:25 -0300

    atualizar-clientes

---
+++ js/atualizar-cliente.js
@@ -0,0 +1,43 @@
+import clientes from './clientesJSON.js';
+
+function getForm() {
+    let matricula = document.getElementById('selectMatricula').value;
+    let nome = document.getElementById('atualiza-nome').value;
+    let telefone = document.getElementById('atualiza-fone').value;
+    let endereco = document.getElementById('atualiza-endereco').value;
+    let filhos = document.getElementById('atualiza-filhos').value;
+    let rendaFamiliar = document.getElementById('atualiza-renda-familiar').value;
+
+    return {
+        matricula: matricula,
+        nome: nome,
+        telefone: telefone,
+        endereco: endereco,
+        filhos: filhos,
+        rendaFamiliar: rendaFamiliar
+    }
+}
+
+function atualizaCliente(matricula, nome, telefone, endereco, filhos, rendaFamiliar) {     
+    const novoCliente = {
+        "matricula": matricula,
+        "nome": nome,
+        "fone": telefone,
+        "endereco": endereco,
+        "filhos": filhos,
+        "rendaFamiliar": rendaFamiliar
+    }
+    
+    let index = clientes.findIndex(cliente => cliente.matricula === matricula);
+    clientes[index] = novoCliente;
+}
+
+document.getElementById("btn-adicionar-cliente").addEventListener("click", function(event){
+    event.preventDefault();
+    const form = getForm();
+
+    const { matricula, nome, telefone, endereco, filhos, rendaFamiliar } = form;
+
+    atualizaCliente(matricula, nome, telefone, endereco, filhos, rendaFamiliar);
+    window.alert('Cliente atualizado com sucesso.');
+});
